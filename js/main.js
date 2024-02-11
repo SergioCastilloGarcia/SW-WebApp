09c55082bbed2f39cbfdc4f5a4a7d89476c9d645
Sergio Castillo <sergiocastillogarci@gmail.com>
2024-02-11 20:03:56 +0100

    Simulacion de obtencion de resultados desde la API

--- js/main.js
+++ js/main.js
@@ -3,11 +3,7 @@ $(document).ready(function () {
     //Comprobamos que esta logeado
     isLogin();
 
-    //Funcionalidad de cada card
-    addDraggable($(".card"));
-
-    //Funcionalidad de las modales
-    $(".card").click(clickCard);
+    getGames();
 
     //Funcionalidad de cada columna
     $(".column").droppable({
@@ -64,20 +60,13 @@ function clickCard() {
 // Simular resultados de búsqueda
 function search() {
     var searchTerm = $(this).val().toLowerCase();
-    var $searchResults = $("#searchResults");
-    $searchResults.empty();
+    var searchResults = $("#searchResults");
+    searchResults.empty();
 
     // Simulamos búsqueda, aquí debes incluir tu lógica de búsqueda real
-    var results = ["Tarea 1", "Tarea 2", "Tarea 3", "Tarea 4"];
+    var results = getResultList(searchTerm);
 
-    results.forEach(function (result, i) {
-        if (result.toLowerCase().includes(searchTerm) && !isGameAdded(i)) {
-            var resultItem = $("<div class='card' id=" + i + "> <img src='img/placeholder.jpg' />" + result + "</div>");
-            addDraggable(resultItem);
-            resultItem.click(clickCard);
-            $searchResults.append(resultItem);
-        }
-    });
+    agregarCards(results)
 }
 
 //Hacer draggable un item
@@ -122,4 +111,51 @@ function eliminar() {
 
     console.log("Eliminado: %s, %s", modalGameId, username)
 
+}
+
+//Obtiene los juegos y los pinta
+function getGames() {
+    var games = getGamesList();
+    agregarCards(games);
+}
+//Obtiene los juegos de la api
+function getResultList(searchTerm) {
+    //TODO llamar a API
+    var username = getCookie("username");
+    return [
+        { id: 11, title: "Kirby", img: "https://uvejuegos.com/img/caratulas/21119/Copia%20de%20kirby-mouse-attack-ds.jpg" },
+        { id: 22, title: "Sonic", img: "https://images.cdn2.buscalibre.com/fit-in/360x360/71/59/7159dc0a2cf1c004783b55eff8b3481f.jpg" },
+        { id: 33, title: "Pokemon Ranger", img: "https://uvejuegos.com/img/caratulas/16646/pokemon-ranger-dsG.jpg" }
+    ];
+}
+//Obtiene los juegos de la api
+function getGamesList() {
+    //TODO llamar a API
+    var username = getCookie("username");
+    return [
+        { id: 88, title: "Mario Kart", img: "https://uvejuegos.com/img/caratulas/7116/Caja_NDS_MarioKart[1].jpg", state: "todo" },
+        { id: 78, title: "Mario 64", img: "https://uvejuegos.com/img/caratulas/7113/super_mario_64_ds_eur.jpg", state: "done" },
+        { id: 98, title: "Mario y Luigi", img: "https://uvejuegos.com/img/caratulas/15406/BajandochemsNDS.jpg", state: "inProgress" }
+    ];
+}
+
+//Agrega las cards
+function agregarCards(games) {
+    games.forEach(function (game) {
+        if (!isGameAdded(game.id)) {//evitamos duplciados
+            const item = $(`<div class="card" id="${game.id}">
+                        <img src="${game.img}" />
+                        ${game.title}
+                    </div>`);
+
+            addDraggable(item);
+            item.click(clickCard);
+            const elementoDestino = $("#" + (game.state ? game.state : "searchResults"));//lo añade a la columa con el state necesario, por defecto en el buscadaor
+            if (elementoDestino.length > 0) {
+                elementoDestino.append(item);
+            } else {
+                console.error(`No se encontró el elemento destino para el estado ${game.state ? game.state : "searchResults"}`);
+            }
+        }
+    });
 }
