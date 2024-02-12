347fe1f56f93a4515497b35b273c942ca8ebe9d3
Sergio Castillo García <sergiocastillogarci@gmail.com>
2024-02-12 19:48:13 +0100

    categorias

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-//Comprobamos que esta logeado
-isLogin();
+
 var modalGameId;//referencia al ide del juego que abrio la modal
 $(document).ready(function () {
-
+    //Comprobamos que esta logeado
+    isLogin();
     getGames();
-
+    getCategories();
     //Funcionalidad de cada columna
     $(".column").droppable({
         accept: ".card",
@@ -15,6 +15,8 @@ $(document).ready(function () {
 
     //Funcionalidad del buscador
     $("#search").on("input", search);
+    //Funcionalidad de la categorias
+    $("#category").on("change", search);
 });
 
 //Función que permite que las columnas acepten cards
@@ -137,6 +139,7 @@ function getGames() {
 function getResultList(searchTerm) {
     //TODO llamar a API
     var username = getCookie("username");
+    var category = $("#category").val();
     return [
         { id: 11, title: "Kirby", img: "https://uvejuegos.com/img/caratulas/21119/Copia%20de%20kirby-mouse-attack-ds.jpg" },
         { id: 22, title: "Sonic", img: "https://images.cdn2.buscalibre.com/fit-in/360x360/71/59/7159dc0a2cf1c004783b55eff8b3481f.jpg" },
@@ -153,7 +156,30 @@ function getGamesList() {
         { id: 98, title: "Mario y Luigi", img: "https://uvejuegos.com/img/caratulas/15406/BajandochemsNDS.jpg", state: "inProgress" }
     ];
 }
+//Obtiene y añade las categorias
+function getCategories() {
+    var categories = getCategoriesList();
+    // Seleccionamos el elemento select
+    var select = $("#category");
 
+    // Iteramos sobre la lista
+    $.each(categories, function (index, category) {
+        // Creamos un elemento option con el valor y el texto correspondientes
+        var option = $("<option>").attr("value", category.slug).text(category.name);
+
+        // Agregamos el elemento option al select
+        select.append(option);
+    });
+}
+//Obtiene los juegos de la api
+function getCategoriesList() {
+    //TODO llamar a API
+    return [
+        { id: 1, name: "Plataforma", slug: "plataforma" },
+        { id: 2, name: "Carreras", slug: "carreras" },
+        { id: 3, name: "Deportes", slug: "deportes" }
+    ];
+}
 //Agrega las cards
 function agregarCards(games) {
     games.forEach(function (game) {
